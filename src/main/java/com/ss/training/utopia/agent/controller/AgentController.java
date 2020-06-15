c37b31f88d142a1c1bc173708705d805b8ec7ba5
trevorhuis-smoothstack <trevor.huis@Smoothstack.com>
2020-06-15 17:24:26 -0500

    read flights with tests

--- src/main/java/com/ss/training/utopia/agent/controller/AgentController.java
+++ src/main/java/com/ss/training/utopia/agent/controller/AgentController.java
@@ -2,7 +2,6 @@ package com.ss.training.utopia.agent.controller;
 
 import java.util.List;
 
-import com.ss.training.utopia.agent.entity.Airport;
 import com.ss.training.utopia.agent.entity.Booking;
 import com.ss.training.utopia.agent.service.AgentService;
 
@@ -11,11 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 /**
@@ -27,19 +23,6 @@ public class AgentController {
 
     @Autowired
 	AgentService service;
-
-    @GetMapping(path="/airport")
-    public ResponseEntity<Airport[]> getAllAirports() {
-		List<Airport> airportList = null;
-		Airport[] airportArray = null;
-		HttpStatus status = HttpStatus.OK;
-		airportList = service.readAirports();
-		if (airportList == null) // no airports exist in the database
-			status = HttpStatus.NO_CONTENT;
-		else
-        airportArray = airportList.toArray(new Airport[airportList.size()]);
-		return new ResponseEntity<Airport[]>(airportArray, status);
-	}
 
 	@GetMapping(path="/bookings/{agentId}")
     public ResponseEntity<Booking[]> getAllBookingsByAgent(@PathVariable long agentId) {
@@ -54,27 +37,4 @@ public class AgentController {
 		return new ResponseEntity<Booking[]>(bookingArray, status);
 	}
 
-	@PutMapping(path="/booking")
-	public ResponseEntity<Booking> cancelBooking(@RequestBody Booking booking) {
-		HttpStatus status = HttpStatus.BAD_REQUEST;
-		if(booking == null || booking.getTravelerId() == null || booking.getBookerId() == null || booking.getStripeId() == null)
-				return new ResponseEntity<Booking>(booking, HttpStatus.BAD_REQUEST);
-
-		service.cancelBooking(booking);
-		status = HttpStatus.OK;
-		
-		return new ResponseEntity<Booking>(booking, status);
-	}
-
-	@PostMapping(path="/booking")
-	public ResponseEntity<Booking> createBooking(@RequestBody Booking booking) {
-		HttpStatus status = HttpStatus.BAD_REQUEST;
-		if(booking == null || booking.getTravelerId() == null || booking.getBookerId() == null || booking.getStripeId() == null)
-				return new ResponseEntity<Booking>(booking, HttpStatus.BAD_REQUEST);
-
-		service.createBooking(booking);
-		status = HttpStatus.CREATED;
-		
-		return new ResponseEntity<Booking>(booking, status);
-	}
 }
--- src/test/java/com/ss/training/utopia/agent/ServiceTestSuite.java
+++ src/test/java/com/ss/training/utopia/agent/ServiceTestSuite.java
@@ -1,15 +1,10 @@
 package com.ss.training.utopia.agent;
-import static org.junit.Assert.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.sql.Timestamp;
-import java.time.Instant;
 import java.util.List;
 
 import com.ss.training.utopia.agent.dao.BookingDAO;
-import com.ss.training.utopia.agent.dao.FlightDAO;
 import com.ss.training.utopia.agent.entity.Booking;
-import com.ss.training.utopia.agent.entity.Flight;
 import com.ss.training.utopia.agent.service.AgentService;
 
 import org.junit.Test;
@@ -26,75 +21,30 @@ H2TestProfileJPAConfig.class})
 public class ServiceTestSuite {
     
 	@Autowired private BookingDAO bookingDao;
-    
-    @Autowired private FlightDAO flightDAO;
 
     @Autowired private AgentService service;
     
-    
-    @Test
-	public void cancelBooking() {
-        //Longs
-        Long oneLong = (long) 1;
-        Long twoLong = (long) 2;
-
-        // Times
-        final Long HOUR = (long) 3_600_000;
-        Long now = Instant.now().toEpochMilli();
-        Timestamp future = new Timestamp(now + HOUR);
-
-        // Future flight and booking - Booker ID: 1, only cancellable flight by user 1
-        Flight futureFlight = new Flight(oneLong, twoLong, future, oneLong, null, null);
-        Booking cancellableBooking = new Booking(oneLong, oneLong, oneLong, true, null);
-
-        flightDAO.save(futureFlight);
-        bookingDao.save(cancellableBooking);
-
-        List<Booking> foundBookings;
-        foundBookings = bookingDao.findCancellable(oneLong);
-        assertNotNull(foundBookings);
-		assertEquals(foundBookings.size(), 1);
-
-        Booking cancelledBooking = service.cancelBooking(cancellableBooking);
-        
-        assertEquals(cancelledBooking.getActive(), false);
-        foundBookings = bookingDao.findCancellable(oneLong);
-        assertNotNull(foundBookings);
-		assertEquals(foundBookings.size(), 0);
-    }
-    
-    @Test
-	public void createBooking() {
-        //Longs
-        Long oneLong = (long) 1;
-        Long twoLong = (long) 2;
-
-        // Times
-        final Long HOUR = (long) 3_600_000;
-        Long now = Instant.now().toEpochMilli();
-        Timestamp future = new Timestamp(now + HOUR);
-
-        // Future flight and booking - Booker ID: 1, only cancellable flight by user 1
-        Flight flight = new Flight(oneLong, twoLong, future, oneLong, null, null);
-        Booking newBooking = new Booking(oneLong, oneLong, oneLong, true, null);
-
-        flightDAO.save(flight);
-
-        List<Booking> foundBookings;
-        foundBookings = bookingDao.findCancellable(oneLong);
-        assertNotNull(foundBookings);
-		assertEquals(foundBookings.size(), 0);
-
-        service.createBooking(newBooking);
-
-        foundBookings = bookingDao.findCancellable(oneLong);
-        assertNotNull(foundBookings);
-		assertEquals(foundBookings.size(), 1);
-    }
-    
     @Test
 	public void readBookingsByAgent() {
+        Booking bookingByAgent = new Booking((long) 1, (long) 1, (long) 1, true, null);
+        Booking bookingByAgent2 = new Booking((long) 2, (long) 1, (long) 1, true, null);
+        Booking bookingNotByAgent = new Booking((long) 3, (long) 1, (long) 2, true, null);
 
+        bookingDao.save(bookingByAgent);
+        bookingDao.save(bookingByAgent2);
+        bookingDao.save(bookingNotByAgent);
+
+        List<Booking> bookingsByAgent = service.readAgentBookings((long) 1);
+
+        assertEquals(bookingsByAgent.size(), 2);
+    }
+
+    @Test
+	public void readBookingsByAgentNoBookings() {
+
+        List<Booking> bookingsByAgent = service.readAgentBookings((long) 1);
+
+        assertEquals(bookingsByAgent.size(), 0);
     }
 
 }
