7f03e41a24ec3e018e580ded4b5c2e7c16ecf578
trevorhuis-smoothstack <trevor.huis@Smoothstack.com>
2020-06-17 14:21:05 -0500

    able to create new user, shameless stealing from Justin

--- src/main/java/com/ss/training/utopia/agent/AgentApplication.java
+++ src/main/java/com/ss/training/utopia/agent/AgentApplication.java
@@ -2,11 +2,12 @@ package com.ss.training.utopia.agent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 /**
  * @author Trevor Huis in 't Veld
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class AgentApplication {
 
 	public static void main(String[] args) {
