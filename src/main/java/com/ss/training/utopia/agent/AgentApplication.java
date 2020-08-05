1d0e78478652ec43eff6f1ee776588a3651df78f
trevorhuis <trevor.huis@smoothstack.com>
2020-08-05 15:46:28 -0500

    Swagger API ended

--- src/main/java/com/ss/training/utopia/agent/AgentApplication.java
+++ src/main/java/com/ss/training/utopia/agent/AgentApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 /**
  * @author Trevor Huis in 't Veld
  */
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@EnableSwagger2
 public class AgentApplication {
 
 	public static void main(String[] args) {
